7b9cb482eb40040fd1678961cfd2adaed316279c
Julio-Cevallos <juliocevallosv.11@gmail.com>
2026-01-04 00:15:45 -0500

    Add superuser creation command

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Ejecutar migraciones
 python manage.py migrate
+
+# Recolectar archivos estáticos
+python manage.py collectstatic --no-input
+
+# Crear superusuario automáticamente
+python manage.py create_superuser
